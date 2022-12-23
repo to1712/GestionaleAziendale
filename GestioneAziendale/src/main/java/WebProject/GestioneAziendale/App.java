3e10fd43ee0bb0c7a0af75876f4b91a38b7f9653
to1712 <toto.orrico@gmail.com>
2022-12-23 16:08:53 +0100

    commit

--- GestioneAziendale/src/main/java/WebProject/GestioneAziendale/App.java
+++ GestioneAziendale/src/main/java/WebProject/GestioneAziendale/App.java
@@ -1,13 +1,21 @@
 package WebProject.GestioneAziendale;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+ 
+
+@SpringBootApplication
+@ServletComponentScan
+public class App {
+
+ 
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
+
+ 
+
 }
