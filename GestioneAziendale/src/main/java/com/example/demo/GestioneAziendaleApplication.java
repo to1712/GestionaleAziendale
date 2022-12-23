274eb23ed3c23b450f0ea39bbab7d699e3c59b1e
Fran2909 <pantusa_francesco@libero.it>
2022-12-23 18:38:31 +0100

    ddddd
    dio ermellino

--- GestioneAziendale/src/main/java/com/example/demo/GestioneAziendaleApplication.java
+++ GestioneAziendale/src/main/java/com/example/demo/GestioneAziendaleApplication.java
@@ -8,7 +8,7 @@ public class GestioneAziendaleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestioneAziendaleApplication.class, args);
-		System.out.println("DIO PORCO");
+		System.out.println("DIO ERMELLINO");
 	}
 
 }
