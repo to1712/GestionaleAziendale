4e470e4afa2e0090234b9a0ffcc62c95e83ea0c8
Fran2909 <pantusa_francesco@libero.it>
2022-12-27 10:15:15 +0100

    upload template

--- GestioneAziendale/src/main/java/com/example/demo/GestioneAziendaleApplication.java
+++ GestioneAziendale/src/main/java/com/example/demo/GestioneAziendaleApplication.java
@@ -8,7 +8,6 @@ public class GestioneAziendaleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestioneAziendaleApplication.class, args);
-		System.out.println("DIO ERMELLINO");//ccocs
 	}
 
 }
