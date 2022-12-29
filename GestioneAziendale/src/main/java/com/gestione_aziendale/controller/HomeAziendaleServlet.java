d4f8eaeced5c8ac56eba152a31ed1c70015703ed
Fran2909 <pantusa_francesco@libero.it>
2022-12-29 19:34:21 +0100

    non funziona una sega

---
+++ GestioneAziendale/src/main/java/com/gestione_aziendale/controller/HomeAziendaleServlet.java
@@ -0,0 +1,26 @@
+package com.gestione_aziendale.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("")
+public class HomeAziendaleServlet extends HttpServlet {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession session = req.getSession();
+		System.out.println(session.getId());
+
+		resp.sendRedirect("/index.html");
+	}
+}
