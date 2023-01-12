9ad55bc0931b311f6224f45371b31a2f1d9c8ba1
to1712 <toto.orrico@gmail.com>
2023-01-12 18:56:55 +0100

    commit

--- GestioneAziendale/src/main/java/com/gestione_aziendale/controller/UtentiServizi.java
+++ GestioneAziendale/src/main/java/com/gestione_aziendale/controller/UtentiServizi.java
@@ -13,17 +13,15 @@ import jakarta.servlet.http.HttpSession;
 @RestController
 @CrossOrigin("http://localhost:4200")
 public class UtentiServizi {
-	public HttpSession session;
-	public void setSession(HttpSession session) {
-		this.session=session;
-	}
 	@PostMapping("/getUtente")
 	public Utente getUtente(HttpServletRequest req ) {
 		String [] sessionIdParam=req.getQueryString().split("&")[0].split("=");
 		String sessionId=sessionIdParam[1];
+		//System.out.println(session.getId());
 		if(req.getServletContext().getAttribute(sessionId)!=null) {
-			Utente u=(Utente) session.getAttribute("user");
-			Utente utente=DBManager.getInstance().getUtenteDao().findByPrimaryKey(u.getEmail());
+			Utente utente= DBManager.getInstance().getUtenteDao().findByPrimaryKey(HomeAziendaleServlet.s);
+			//System.out.println(HomeAziendaleServlet.s);
+			System.out.println(utente.getEmail());
 			return utente;
 		}
 		return null;
